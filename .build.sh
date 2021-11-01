3ce3abe47b5a860272057f0c81c6d984cbedece6
Asna Guo <asnaguo@gmail.com>
2021-11-01 15:42:45 +0700

    [deploy] bismillah init

--- .build.sh
+++ .build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
             echo "build api image"
             echo 9pzefevmYkxQ7g6DNoEx | docker login registry.gitlab.com -u viavallen --password-stdin
-            docker build -t registry.gitlab.com/viavallen/kentang-goreng/aimi_mothership_dashboard:latest .
-            docker push registry.gitlab.com/viavallen/kentang-goreng/aimi_mothership_dashboard:latest
+            docker build -t registry.gitlab.com/viavallen/kentang-goreng/talent_talent_dashboard:latest .
+            docker push registry.gitlab.com/viavallen/kentang-goreng/talent_talent_dashboard:latest
             
